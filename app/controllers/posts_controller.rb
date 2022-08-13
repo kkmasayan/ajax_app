c90c5a043ae6e673d3298be8a8054e39f85714d2
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2022-08-13 16:31:41 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,10 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
 
-   # コメントアウトする
-  # def new
-  # end
+
 
   def create
-    Post.create(content: params[:content])
+    post = Post.create(content: params[:content])
     render json:{ post: post }
   end
 end
